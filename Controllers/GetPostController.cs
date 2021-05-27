a77997b0950914aa54607c19b3440cd600eeabf5
Datakda <gagan9393@gmail.com>
2021-05-27 14:01:51 +0300

    Добавьте файлы проекта.

---
+++ Controllers/GetPostController.cs
@@ -0,0 +1,40 @@
+﻿using GetPost.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GetPost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GetPostController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+           MinMax rnd = new MinMax();
+
+            WebRequest request = WebRequest.Create("https://testgetpost1241414.000webhostapp.com/rnd.txt");
+            WebResponse response = request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    line = reader.ReadLine();
+
+                    rnd = JsonSerializer.Deserialize<MinMax>(line); ;
+
+                }
+            }
+            response.Close();
+            return $"Min = {rnd.Min}, Max = {rnd.Max}";
+
+        }
+    }
+}
